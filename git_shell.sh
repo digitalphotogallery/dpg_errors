f01d6428e3a5adfcea56b2e633eec8cebc514ba8
Andrew Kendall <andrew@andrewkendall.com>
2012-11-24 03:26:49 +0100

    2012-11-24 - 03:26:49

---
+++ git_shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASEDIR=`dirname "$0"`
+cd "$BASEDIR"
+git add .
+git add -u
+DATE=`date "+%Y-%m-%d - %H:%M:%S"`
+git commit -m "$DATE"
+git fsck --full
+git gc --aggressive
+git push origin master
